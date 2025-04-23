7b0da82f77030c00bcd85eacd7fa84c4d8985cc3
Terje Lafton <terje@lafton.io>
2025-04-23 08:16:29 +0200

    Add vertical spacing between enum variants

--- src/error.rs
+++ src/error.rs
@@ -8,10 +8,13 @@ use crate::Rule;
 pub enum ParserError {
     #[error("entrypoint must be a YANG module, not submodule")]
     InvalidParserEntrypoint,
+
     #[error("invalid YANG file")]
     ParseError(#[from] pest::error::Error<Rule>),
+
     #[error("invalid input file")]
     InvalidFile(#[from] io::Error),
+
     #[error("included file has to be a submodule, not module: {0}")]
     InvalidInclude(String),
 }
